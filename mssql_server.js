d8c9e13519c442e2eb5a4a704f2af8de83af28cb
willowsenator <willowsenator@gmail.com>
2023-10-07 03:32:46 +0200

    add client for mssql_server

---
+++ mssql_server.js
@@ -0,0 +1,33 @@
+const mssql = require("mssql");
+const config = {
+    user: "sa",
+    password: "Mssql_2023.",
+    database: "northwind",
+    server: "127.0.0.1",
+    pool: {
+        min: 0,
+        max: 10,
+        idleTimeoutMillis: 30000
+    },
+    options:{
+        encrypt: true,
+        trustServerCertificate: true
+    }
+}
+
+async function query(sql){
+    try {
+        await mssql.connect(config);
+        const result = await mssql.query(sql);
+        return result;    
+    } catch (error) {
+        return {error};
+    }
+    
+}
+
+query("select * from orders").then(res=>{
+    console.log(res);
+}).catch(err => {
+    console.error(err);
+})
